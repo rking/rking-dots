dda7dfcb1bcb5736e350b2d461315b5c6ce8a060
☈king <rking@sharpsaw.org>
2012-06-13 03:12:22 -0600

    Add 3 column layout.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,13 @@
--- import XMonad.Layout.ThreeColumns
+import XMonad
 
--- myLayout = ThreeCol 1 (3/100) (1/2) ||| ThreeColMid 1 (3/100) (1/2) ||| etc..
--- main = xmonad defaultConfig { layoutHook = myLayout }
--- Which one? ↓↑
--- main = xmonad $ defaultConfig { layoutHook = mylayoutHook }
+--- Note: This requires xmonad-contrib (on Gentoo, at least)
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Accordion
+import XMonad.Layout.NoBorders
+
+myLayout = ThreeCol 1 (3/100) (1/2) ||| ThreeColMid 1 (3/100) (1/2) ||| Full ||| noBorders (tabbed shrinkText defaultTheme) ||| Accordion
+
+main = xmonad defaultConfig { layoutHook = myLayout }
+-- TODO
+-- modMask = mod3Mask
