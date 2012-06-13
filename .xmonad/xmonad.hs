bac1dedc1d5a4d4506cd862793236a76088cd00a
rking <rking@sharpsaw.org>
2012-06-13 07:15:05 -0500

    Make Alt+Shift+Enter give me a urxvt.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,6 +8,8 @@ import XMonad.Layout.NoBorders
 
 myLayout = ThreeCol 1 (3/100) (1/2) ||| ThreeColMid 1 (3/100) (1/2) ||| Full ||| noBorders (tabbed shrinkText defaultTheme) ||| Accordion
 
-main = xmonad defaultConfig { layoutHook = myLayout }
+main = xmonad defaultConfig
+    { layoutHook = myLayout
+    , terminal = "LANG=en_US.utf8 urxvt" }
 -- TODO
 -- modMask = mod3Mask
