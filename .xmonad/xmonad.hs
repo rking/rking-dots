7948870e8c795c07cebdf15ed161c5858a590573
☈king <rking@sharpsaw.org>
2012-06-17 21:45:45 -0600

    Use mod3 for modMask.
    I really want this "system" to work, so that it's robust enough to belong in
    x-dots, but oh well. TODO.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,11 +5,19 @@ import XMonad.Layout.ThreeColumns
 import XMonad.Layout.Tabbed
 import XMonad.Layout.Accordion
 import XMonad.Layout.NoBorders
+import System.Process
+import System.Exit
 
 myLayout = ThreeCol 1 (3/100) (1/2) ||| ThreeColMid 1 (3/100) (1/2) ||| Full ||| noBorders (tabbed shrinkText defaultTheme) ||| Accordion
 
+-- -- If xmodmap has, for example, mapped Caps_Lock to mod3, then use that:
+-- myModMask = do retval <- system "xmodmap | grep '^mod3\\s*\\S*' >/dev/null"
+--                if retval == ExitSuccess
+--                  then mod3Mask
+--                  else mod1Mask
+myModMask = mod3Mask
+
 main = xmonad defaultConfig
-    { layoutHook = myLayout
-    , terminal = "LANG=en_US.utf8 urxvt" }
--- TODO
--- modMask = mod3Mask
+            { layoutHook = myLayout
+            , modMask = myModMask
+            , terminal = "LANG=en_US.utf8 urxvt" }
